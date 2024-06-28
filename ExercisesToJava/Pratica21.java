b214401aea3eacec400aa239a42ba44991563893
babislz <gabriela.laureano2007@gmail.com>
2024-06-28 11:08:23 -0300

    3 exercises

---
+++ ExercisesToJava/Pratica21.java
@@ -0,0 +1,33 @@
+package ExercisesToJava;
+
+public class Pratica21 {
+    public static void main(String[] args) {
+        int num1 = 151, num2 = 98;
+        System.out.println(num1>num2?"Verdadeiro":"Falso");
+    }
+}
+
+//------------------------------| Código em C |------------------------------//
+
+// #include<stdio.h>
+// #include<stdlib.h>
+// #include<locale.h>
+
+// int main(){
+// 	setlocale(LC_ALL,"Portuguese");
+	
+// 	/*fflush(stdin); <comando para limpar bd de entredadas,
+// 	 limpar o enter dado pelo usuario>*/
+// 	//int x; declara��o de varialvel x como inteira
+	
+
+// 		if (51>50){
+// 		printf("verdadeiro \n");
+// 		}
+// 		else{
+// 			printf("falso \n");
+// 		}
+
+// 	system("pause");
+// 	return 0;
+// }
---
+++ ExercisesToJava/Pratica8.java
@@ -0,0 +1,29 @@
+package ExercisesToJava;
+
+import java.util.Scanner;
+
+public class Pratica8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Qual é a idade do aluno? ");
+        int age = sc.nextInt();
+        System.out.println("A idade informada foi: " + age + " anos");
+        sc.close();
+    }
+}
+
+//------------------------------| Código em C |------------------------------//
+
+// #include<stdio.h>
+// #include<stdlib.h>
+// //exercicio 8 aula pr�tica
+// int main(){
+// 	int age;
+	
+// 	printf("Qual a idade do aluno:");
+// 	scanf("%d", &age);
+// 	printf("A idade Informada foi: %d \n", age);
+	
+// 	system("pause");
+// 	return 0;
+// }
